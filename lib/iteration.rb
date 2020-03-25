d70a7ece232956ea953223651870910a8218e0d2
Jennifer Paquette <jennidemuir@gmail.com>
2020-03-25 22:15:25 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -9,7 +9,7 @@ def join_ingredients(src)
 ingredients = []
 index = 0 
 while index < src.count do 
-  ingredients << ("I love #{src[index][0]}")
+  ingredients << ("I love #{src[index][0]} and #{src[index][1] on my pizza")
  
  
  end
