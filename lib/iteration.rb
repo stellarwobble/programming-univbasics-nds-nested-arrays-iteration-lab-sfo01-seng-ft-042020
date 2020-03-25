4197dedca33e8d9419a90cbfca120bf7c6572bf2
Jennifer Paquette <jennidemuir@gmail.com>
2020-03-25 22:32:45 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -35,18 +35,6 @@ def find_greater_pair(src)
   
   
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
 end
 
 def total_even_pairs(src)
@@ -57,4 +45,9 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
+  
+  
+  
+  
+  
 end
